b074a44f7e9edd14188c0146d936d2f8a279e4f7
elliot estrade <esstsolutions@gmail.com>
2026-03-28 18:35:50 +0100

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/conciergeries",
+        destination: "/sites-vitrines/conciergerie",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
